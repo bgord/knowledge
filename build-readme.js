ba9c2273c16f1b76988b1b05c6a6f94f35a32b78
Bartosz Gordon <bartosz.gordon@sealcode.org>
2019-12-22 16:40:10 +0100

    Add build readme script

---
+++ build-readme.js
@@ -0,0 +1,47 @@
+const { promises: fs } = require("fs");
+
+const pathsToSkip = [
+  ".git",
+  ".gitignore",
+  "README.md",
+  "roadmap.md",
+  "build-readme.js",
+  "libs-and-techniques.md"
+];
+
+async function main() {
+  const allFiles = await fs.readdir(".");
+  const mdFiles = allFiles.filter(file => !pathsToSkip.includes(file));
+
+  const data = {};
+
+  for (const filename of mdFiles) {
+    const file = await fs.readFile(filename);
+
+    const titles = file
+      .toString()
+      .split("\n")
+      .filter(line => line.startsWith("**") && line.endsWith("**"))
+      .map(titleLine => titleLine.substring(2, titleLine.length - 2));
+
+    data[filename] = titles;
+  }
+
+  const total = Object.values(data)
+    .map(file => file.length)
+    .reduce((a, b) => a + b, 0);
+
+  let readme = "# Knowledge\n\n";
+
+  readme += `Total entries: ${total}\n`;
+
+  for (const [filename, titles] of Object.entries(data)) {
+    const [category] = filename.split(".");
+    readme += `\n### ${category}\n`;
+    readme += titles.map(title => `- ${title}\n`).join("");
+  }
+
+  await fs.writeFile("README.md", readme);
+}
+
+main();
